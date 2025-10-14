4f1c1e3542b0bf591c6ed2e697651fe680178266
Johan Rong <59508244+johron@users.noreply.github.com>
2025-10-14 09:26:46 +0200

    yes

--- lib/page/playlist_page.dart
+++ lib/page/playlist_page.dart
@@ -43,9 +43,9 @@ class _PlaylistPageState extends State<PlaylistPage> {
             direction: Axis.horizontal,
             children: [
               Rounded(radius: 10, child: Image.network(
-                playlist.iconPath ?? 'https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999',
-                width: 175,
-                height: 175,
+                playlist.iconPath ?? getMissingAlbumArtPath(),
+                width: 150,
+                height: 150,
                 fit: BoxFit.cover,
               )),
               Expanded(  // Add this Expanded widget
